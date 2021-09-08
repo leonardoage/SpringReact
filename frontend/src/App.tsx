bed518f6941d8f7797ff5c78d5894e8d046f7481
Leonardo Age Netto <leonardoage@gmail.com>
2021-09-07 23:43:21 -0300

    Alterando charset dos arquivos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,21 +13,22 @@ function App() {
         <h1 className="text-primary py-3"> Dashboard Vendas </h1>
 
         <div className="row px-3">
-        <div className="col-sm-6">
+
+          <div className="col-sm-6">
             <h5 className="text-center text-secondary"> Taxa de sucesso (%) </h5>
             <BarChart />
           </div>
+
           <div className="col-sm-6">
             <h5 className="text-center text-secondary"> Vendas por Vendedores</h5>
             <DonutChart />
           </div>
-        </div>
-        <div className="py-3">
-          <h2 className="text-primary">Todas as vendas</h2>
-          <DataTable />
+          <div className="py-3">
+            <h2 className="text-primary">Todas as vendas</h2>
+            <DataTable />
+          </div>
         </div>
       </div>
-
       <Footer />
     </>
   );
--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -11,7 +11,7 @@ const BarChart = () => {
 
     const mockData = {
         labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'PadmÃ©']
         },
         series: [
             {
