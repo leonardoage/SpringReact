f322500c9f22dd7d6708c960972140015987938a
Leonardo Age Netto <leonardoage@gmail.com>
2021-09-09 21:48:09 -0300

    Layers

---
+++ backend/src/main/java/com/jsystems/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.jsystems.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jsystems.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
